63ff46d544baae3f14f892e61387e55615beeedb
Geekthedev <geekthedeveloper@gmail.com>
2024-12-31 15:54:24 +0100

    Added hello.js and hello.py

---
+++ hello.js
@@ -0,0 +1,9 @@
+function user(name, age){
+  let user_name = name;
+  let user_age = age;
+  return user_name, user_age
+}
+
+result = user("Your Name", 18);
+result = user("Any Name", 20);
+console.log(result)
---
+++ hello.py
@@ -0,0 +1,6 @@
+def user_name(string_t):
+  first_name = string_t
+  return first_name
+
+result = user_name("My Name")
+print(result)
